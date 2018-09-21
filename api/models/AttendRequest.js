9a20ada93919c9eb5cf6394859720ee7858de352
N00BEST <marcano.daniel98@gmail.com>
2018-09-21 14:30:17 -0400

    Respaldo

---
+++ api/models/AttendRequest.js
@@ -0,0 +1,62 @@
+/**
+ * AttendRequest.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    realAttentionTime: {
+		type: 'string',
+		columnType: 'datetime'
+	},
+	
+	estimatedTime: {
+		type: 'string',
+		columnType: 'datetime'
+	},
+
+	startedToAttend: {
+		type: 'string',
+		columnType: 'datetime'
+	},
+
+	calification: {
+		type: 'number',
+
+	},
+
+	status: {
+		type: 'string',
+		
+	},
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+	//  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+	
+	points: {
+		collection: 'point',
+		via: 'idZone'
+	},
+
+	locations: {
+		collection: 'location',
+		via: 'idZone'
+	}
+
+  },
+
+};
