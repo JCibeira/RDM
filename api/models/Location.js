20a22b42cebd096229021033b025a5abf34f82de
N00BEST <marcano.daniel98@gmail.com>
2018-09-21 11:39:40 -0400

    Creación de modelos de la BBDD

---
+++ api/models/Location.js
@@ -0,0 +1,48 @@
+/**
+ * Location.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    longitude: {
+      type: 'string',
+      required: true,
+      maxLength: 200
+    },
+
+	latitude: {
+		type: 'string',
+		required: true,
+		maxLength: 200
+	},
+
+	idZone: {
+		model: 'zone'
+	}, 
+
+	directions: {
+		collection: 'direction',
+		via: 'idLocation'
+	}
+	
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
